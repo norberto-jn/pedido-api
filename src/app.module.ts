38d2b8c9edc6b69e2e148cabcf2f64485a9b8ff2
norberto-jn <norbertojunio@gmail.com>
2022-04-13 10:10:03 -0300

    arquivo .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppController } from './app.controller'
 import { FoodModule } from './food/FoodModule'
 import { FoodModel } from './food/src/model/FoodModel'
 
+require('dotenv').config('../.env')
+
 @Module({
     imports: [AppModule, FoodModule, TypeOrmModule.forRootAsync({
         useFactory: () => ({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'nerit',
-            password: null,
+            type: process.env.DB_CONNECTION,
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD == 'NULL' ? null : process.env.DB_PASSWORD,
             database: 'food',
             entities: [FoodModel],
             synchronize: true,
