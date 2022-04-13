59be66cf0a4e856383474bdfe9dd17fb66f8dfc2
norberto-jn <norbertojunio@gmail.com>
2022-04-13 18:44:51 -0300

    ajustes conexao

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,12 @@ require('dotenv').config('../.env')
 @Module({
     imports: [AppModule, FoodModule, TypeOrmModule.forRootAsync({
         useFactory: () => ({
-            type: process.env.DB_CONNECTION,
+            type: 'mysql',
             host: process.env.DB_HOST,
             port: Number(process.env.DB_PORT),
             username: process.env.DB_USERNAME,
             password: process.env.DB_PASSWORD == 'NULL' ? null : process.env.DB_PASSWORD,
-            database: 'food',
+            database: process.env.DB_DATABASE,
             entities: [FoodModel],
             synchronize: true,
             autoLoadEntities: true
